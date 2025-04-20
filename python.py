28d93753a6839323825a605ffb4c81513dc4b107
Kiansharestani <ksharestani@gmail.com>
2025-04-20 13:13:08 +0330

    Update python.py

--- python.py
+++ python.py
@@ -1,10 +1,3 @@
-- 👋 Hi, I’m @Kiansharestani
-- 👀 I’m interested in ...
-- 🌱 I’m currently learning ...
-- 💞️ I’m looking to collaborate on ...
-- 📫 How to reach me...
-- 
-<!---
-Kiansharestani/Kiansharestani is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+print("hello")
+name = input("what is your name?")
+print('welcom'name)
